b821ad1aa33d31c9dbefc30f988ebb7a15c49cb7
Chris Bader <chris.bader@schnapptack.de>
2016-08-25 15:33:59 +0200

    further progress

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import requests
 
-class url_list:
-    def __init__(self, Name):
-        self.name = Name
-        contents = []
-        print "a new URLlist named " + Name + " has been created"
 
-    def get_list_contents(self, url_list):
-        for items in url_list.con:
-            print items
+# create a list of urls
+class UrlList(object):
+    instances = []
 
+    def __init__(self, name):
+        self.name = name
+        UrlList.instances.append(self)
+        print "a new URLlist named " + name + " has been created"
 
-
-
+# return all list instances
+def yield_list_instances():
+    for item in UrlList.instances:
+        return item.name
 
 # returns the http- response code of the targeted url
 def get_statuscode(self,target_url):
@@ -26,16 +27,14 @@ def get_statuscode(self,target_url):
         return "nope"
 
 
-
-
-
 def add_url_to_list(url, list_name):
     list_name.append(url)
     return
 
 
-list_1 = url_list("List1")
-list_1.get_list_contents(list_1)
-#add_url_to_list("http://www.google.com", )
+
+
+list1 = UrlList("asdasd")
+print(yield_list_instances())
 
 
