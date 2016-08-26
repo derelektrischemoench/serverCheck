15451d93f8d99ec9a8115cca4b6666e9f765d98c
Chris Bader <baederchris@gmail.com>
2016-08-26 10:26:03 +0200

    added second server

--- server.js
+++ server.js
@@ -3,8 +3,20 @@
 var http=require('http');
 
 http.createServer(function (req,res) {
-    res.writeHead(404,{'Content-Type':'text/plain'});
-    res.end('Hello world\n');
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('hello world');
+    res.end();
 }).listen(8124,"127.0.0.1");
 
-console.log("Server running @ localhost:8124");
+console.log("Server1 running @ localhost:8124");
+
+//Server2
+var http2=require('http');
+
+http2.createServer(function (req,res) {
+    res.writeHead(404,{'Content-Type':'text/html'});
+    res.write('hello world2');
+    res.end();
+}).listen(8125,"127.0.0.1");
+
+console.log("Server2 running @ localhost:8125");
