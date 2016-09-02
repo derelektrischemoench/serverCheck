175fcdf26b69981357e6ecb7cdf18b3d4d3ae328
Chris Bader <chris.bader@schnapptack.de>
2016-09-02 10:28:44 +0200

    Merge branch 'mymaster'

--- setup.py
+++ setup.py
@@ -8,10 +8,5 @@ setup(
     url='git@github.com:derelektrischemoench/serverCheck.git',
     author='Chris Bader',
     author_email='chris.bader@schnapptack.de',
-<<<<<<< HEAD
     packages=['Mock', 'servercheck', 'tests'],
-=======
-    packages=['servercheck', 'tests', 'requests'],
->>>>>>> f07e30b7aca65bba70da8f085f52b562e42b7565
-    install_requires=['unittest2'],
 )
