40a8cc8cab4a697e2d4c014b9585028293851c16
Chris Bader <chris.bader@schnapptack.de>
2016-09-02 14:57:42 +0200

    modified imports

--- tests/tests.py
+++ tests/tests.py
@@ -1,25 +1,25 @@
 import os, sys
 import unittest
 import mock
-from servercheck.servercheck import *
+import servercheck.servercheck as servercheck
 
 class TestCase(unittest.TestCase):
 
     def test_google_url_is_reachable(self):
-        self.assertFalse(url_is_reachable("http://www.google.de"))
+        self.assertFalse(servercheck.url_is_reachable("http://www.google.de"))
 
     def test_foobar_url_is_not_reachable(self):
-        self.assertFalse(url_is_reachable("http://www.schnapptack.de/this_is_missing_here_123.html"))
+        self.assertFalse(servercheck.url_is_reachable("http://www.schnapptack.de/this_is_missing_here_123.html"))
 
     def test_nonexisting_tld_is_not_reachable(self):
-        self.assertTrue(url_is_reachable("http://www.schnappfuck.de/"))
+        self.assertTrue(servercheck.url_is_reachable("http://www.schnappfuck.de/"))
 
     @mock.patch('servercheck.servercheck.requests.get')
     def test_get_ok(self, mock_get):
         mock_response = mock.Mock()
         mock_response.ok.return_value = True
 
-        self.assertTrue(url_is_reachable("http://www.schnappfuck.de/"))
+        self.assertTrue(servercheck.url_is_reachable("http://www.schnappfuck.de/"))
 
         mock_get.assert_called_once_with("http://www.schnappfuck.de/")
         #1st argument is expected result
