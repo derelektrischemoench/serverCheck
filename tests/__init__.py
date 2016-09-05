443abc21557d9700ff7b83b7c8cca5f59ca511c1
Chris Bader <chris.bader@schnapptack.de>
2016-09-05 14:17:54 +0200

    another try

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,10 +2,5 @@ from __future__ import absolute_import
 import unittest
 from tests.tests import *
 
-def suite():
-    return unittest.TestSuite([
-        unittest.TestLoader().loadTestsFromTestCase(TestCase2)
-    ])
-
 def run_in_jenkins():
-    return unittest.TestRunner(verbosity=2).run(suite())
+    return unittest.main()
