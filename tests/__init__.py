0d6923488e58ae13b16bfdf6f87ea0ce3c7c5005
Chris Bader <chris.bader@schnapptack.de>
2016-09-05 11:51:36 +0200

    added jenkins test suite... lets see if it works

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,11 @@
-__all__ = ['tests', ]
+from __future__ import absolute_import
+import unittest
+from serverCheck.tests.tests import *
 
+def suite():
+    return unittest.TestSuite([
+        unittest.TestLoader().loadTestsFromTestCase(TestCase2)
+    ])
+
+def run_in_jenkins():
+    return unittest.TestRunner(verbosity=2).run(suite())
