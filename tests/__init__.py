e3c4095483aa70db3baf899a863e247873c75fe7
Chris Bader <chris.bader@schnapptack.de>
2016-09-05 14:28:48 +0200

    another try

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 import unittest
-from tests.tests import TestCase, TestCase2
+from tests import TestCase, TestCase2
 
 def run_in_jenkins():
-    TestCase()
-    TestCase2()
     return unittest.main()
